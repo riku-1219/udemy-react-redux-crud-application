16563b891f0d98bbc72f3869f62d206dd1662904
rikuto ui <mdr.crew.1219@gmail.com>
2021-02-06 07:40:36 +0900

    learn prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
   const profiles = [
     { name: "Taro", age: 10 },
     { name: "Hanako", age: 5 },
-    { name: "noName" },
-  ]
+    { name: "NoName", age: 3 },
+  ];
   return (
     <div>
       {profiles.map((profile, i) => {
-        return <User name={profile.name} age={profile.age} key={i} />
+        return <User name={profile.name} age={profile.age} key={i} />;
       })}
     </div>
   );
 };
 
 const User = (props) => {
-  return <div>Hi! I am {props.name}, and {props.age} yers old!</div>;
+  return (
+    <div>
+      Hi! I am {props.name}, and {props.age} yers old!
+    </div>
+  );
 };
 
-User.defaultProps = {
-  age: 1,
-}
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
+};
 
 export default App;
