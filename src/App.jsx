3e2179d1333e9f626a88535a953e9388143aa33b
rikuto ui <mdr.crew.1219@gmail.com>
2021-02-06 05:48:48 +0900

    create class component and functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { Component } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor='bar'>bar</label>
-        <input type='text' onChange={() => console.log("I'm changed.")} />
-      </React.Fragment>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor='bar'>bar</label>
+//         <input type='text' onChange={() => console.log("I'm changed.")} />
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
