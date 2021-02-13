19cc95ca09d10c7b9c8bbe48f32b3662e6efe0c4
rikuto ui <mdr.crew.1219@gmail.com>
2021-02-13 16:16:53 +0900

    implement events index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { increment, decrement } from "../actions";
 
-class App extends React.Component {
+class EventsIndex extends React.Component {
   render() {
     const props = this.props;
     return (
@@ -17,13 +17,9 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state) => ({ value: state.count.value });
-// const mapDispatchToProps = (dispatch) => ({
-//   increment: () => dispatch(increment()),
-//   decrement: () => dispatch(decrement()),
-// });
 
 const mapDispatchToProps = { increment, decrement };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
 
 // export default App;
